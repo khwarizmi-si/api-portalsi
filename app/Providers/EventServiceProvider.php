3485221e8a80d911f8e8fa1bdd6958076feccb44
Azzam Hudiya <hudiya777@gmail.com>
2025-09-21 11:36:54 +0700

    fc

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,10 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Login;
 use App\Listeners\LogSuccessfulLogin;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -18,11 +18,6 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
-
-        // ✅ tambahkan mapping untuk login
-        Login::class => [
-            LogSuccessfulLogin::class,
         ],
     ];
 
