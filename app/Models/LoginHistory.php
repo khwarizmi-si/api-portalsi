a9f1673bd8a9b238018b67556c3f59e4a9876498
Azzam Hudiya <117409466+azzamhaer@users.noreply.github.com>
2025-09-23 23:06:05 +0700

    Update LoginHistory.php fix balikin wkwk

--- app/Models/LoginHistory.php
+++ app/Models/LoginHistory.php
@@ -39,7 +39,7 @@ class LoginHistory extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'id');
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
     }
 
     /**
