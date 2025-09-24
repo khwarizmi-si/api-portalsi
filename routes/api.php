8f5a502033f4c634a668c61cb2fe6a309318f42b
Azzam Hudiya <hudiya777@gmail.com>
2025-09-24 10:28:28 +0700

    add: default banner url at registration

--- routes/api.php
+++ routes/api.php
@@ -66,7 +66,8 @@ Route::post('/register', function (Request $request) {
         'email' => strtolower($request->email),     // 👈 Lowercase email
         'password_hash' => bcrypt($request->password),
         'role' => $request->role ?? 'student',
-        'profile_picture_url' => 'https://api-new.portalsi.com/storage/default-profile.png'
+        'profile_picture_url' => 'https://api-new.portalsi.com/storage/default-profile.png',
+        'banner_url' => 'https://api-new.portalsi.com/storage/default-banner.png'
     ]);
 
     $user->sendEmailVerificationNotification();
