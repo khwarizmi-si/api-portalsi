1482a1915a3f0e0b86dd6d579feffb7a71ab2efb
Azzam Hudiya <hudiya777@gmail.com>
2025-10-10 10:45:08 +0700

    story next pagination

--- routes/api.php
+++ routes/api.php
@@ -499,6 +499,10 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/stories/{id}/viewers', [StoryViewController::class, 'viewers']);
         Route::get('/stories/user/{userId}', [StoryController::class, 'getByUser']);
         Route::get('/stories/my/archived', [StoryController::class, 'myArchivedStories']);
+        Route::get('/stories/feed/paginated', [StoryController::class, 'feedPaginated']);
+        Route::get('/stories/feed/next', [StoryController::class, 'feedNextUser']);
+
+
 
         // Notifications
         Route::patch('/notifications/{id}/read', [NotificationController::class, 'markAsRead']);
